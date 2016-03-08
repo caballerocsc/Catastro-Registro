40e3b40972e32a6b1047d3186f28f01f3e49c27c
caballerocsc <cesarsolau@hotmail.com>
2016-03-08 17:49:46 -0500

    control 08-03-2016
    primera version del software

---
+++ src/modelo/Conexion.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conexion {
+    private static final Logger log = Logger.getLogger(Conexion.class);
+    
+    public Conexion() {
+    }
+
+    String driver = "org.postgresql.Driver";
+    String connectString = "jdbc:postgresql://localhost:5432/cata_reg";
+    String user = "postgres";
+    String password = "123456";
+
+    public Connection getConexion() {
+        Connection con = null;
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(connectString, user, password);
+        } catch (SQLException e) {
+            System.out.println("sqlexcepcion");
+        } catch (ClassNotFoundException e) {
+            System.out.println("classnofoundexception");
+        }
+        return con;
+    }
+
+    /**
+     * Método que se encarga de cerrar el objeto de conexión
+     *
+     * @param cn Objeto de tipo Connection instanciado
+     */
+    public void cerrar(Connection cn) {
+        if (cn != null) {
+            try {
+                //	System.out.println("cierra conexion");
+                cn.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                log.error("No se ha cerrado correctamente la conexion: ", e);
+            }
+        }
+    }
+
+    /**
+     * Método que se encarga de cerrar el objeto de PreparedStatement
+     * @param pst Objeto de tipo PreparedStatement instanciado
+     */
+    public void cerrar(PreparedStatement pst) {
+        if (pst != null) {
+            try {
+                pst.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                 log.error("No se ha cerrado correctamente el PreparedStatement: ", e);
+            }
+        }
+    }
+
+    /**
+     *  Método que se encarga de cerrar el objeto de ResultSet
+     * @param rs Objeto de tipo ResultSet instanciado
+     */
+    @SuppressWarnings("empty-statement")
+    public void cerrar(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                 log.error("No se ha cerrado correctamente el resultset: ", e);
+            }
+        }
+    }
+    
+    
+}
---
+++ src/modelo/Sentencias.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum Sentencias {
+    
+    
+    INSERTARIGACCATASTRO("INSERT INTO Catastro (departamentoidfk,municipioidfk,numpredio,avaluo,sector,manzana,predio,propiedad,matricula,direccion,tipo,estado,fechaRecibido) VALUES\n" +
+        " ('','','','','','','','','','','','','');"),
+    INSERTARIGACREGISTRO("INSERT INTO registro (matriculaori,predialori,direccion,tipo,estado,fecharecibido,municipioidfk) VALUES\n" +
+        " ('','','','','','','');"),
+    INSERTARIGACITR("INSERT INTO ITR (departamentoidfk,municipioidfk,fecharecibido,numpredio,avaluos,sector,manzana,predio,propiedad,circulo,matricula,areaterreno,areaconstruida,direccioncat,direccionreg,crpredial,crmatricula,crdireccion,crdocumento,crnombre) VALUES\n" +
+        " ('','','','','','','','','','','','','','','','','','','');"),
+    INSERTARIGACCATASTROPROPIETARIO("INSERT INTO catastro_propietarios (catastroid_Catastro,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARIGACREGISTROPROPIETARIO(" INSERT INTO registro_propietarios (registroid_registro,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARIGACITRCATPROP(" INSERT INTO itr_cat_propietario (itrid_ITR,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARIGACITRREGPROP(" INSERT INTO itr_reg_propietario (itrid_ITR,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARANTIOQUIACATASTRO(" INSERT INTO catastro_gobAnt (municipioidfk,departamentoidfk,predial,predialcat28,predialcat30,avaluo,corregimiento,matriculaori,direccion,tipo,estado,fecharecibido) VALUES\n" +
+        " ('','','','','','','','','','','','');"),
+    INSERTARANTIOQUIACATASTROPROP("INSERT INTO cat_gobant_prop (catastroid_catastro_gobAnt,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARANTIOQUIAREGISTRO("INSERT INTO registro_gobant (matriculaori,predialori,direccion,tipo,estado,municipioidfk,fecharecibido) VALUES\n" +
+        " ('','','','','','','');"),
+    INSERTARANTIOQUIAREGISTROPROP(" INSERT INTO reg_gobant_prop (registroid_registro_gobant,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARANTIOQUIAITR("INSERT INTO itr_gobant (departamentoidfk,municipioifk,numpredio,predialcat,predialcat28,predialcat30,avaluo,corregimiento,circulo,matricula,areaterreno,areaconstruida,direccioncat,direccionreg,crpredial,crmatricula,crdireccion,crdocumento,crnombre,fecharecibido) VALUES\n" +
+        " ('','','','','','','','','','','','','','','','','','','','');"),
+    INSERTARANTIOQUIAITRCATASTROPROP("INSERT INTO itr_catgobant_prop (itrgobantid_itr_gobant,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARANTIOQUIAITRREGISTROPROP("INSERT INTO itr_reggobant_prop (itrgobantid_itr_gobant,propietarioid_propietarios) VALUES\n" +
+        " ('','');"),
+    INSERTARMEDELLINCATASTRO("INSERT INTO catastro_medellin (idpredio,numpredial,areaterreno,areaconstruida,direccion,tipopredio) VALUES\n" +
+        " ('','','','','','');"),
+    INSERTARMEDELLINREGISTRO(" INSERT INTO registro_medellin (circulo,matricula,direccion,tipopredio) VALUES\n" +
+        " ('','','','');"),
+    INSERTARMEDELLINITR("INSERT INTO itr_medellin (idpredio,numpredio,matricula,areaterreno,areaconstruida,direccioncat,direccionreg,tipopredio,crpredial,crmatricula,crdireccion,crdocumento,crnombre,fecharecibido) VALUES\n" +
+        " ('','','','','','','','','','','','','','');");
+    
+
+    private Sentencias(String sentencia) {
+        this.sentencia=sentencia;
+    }
+    
+    private final String sentencia;
+    
+    public String getSentencia(){
+        return sentencia;
+    }
+}
